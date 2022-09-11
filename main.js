e666e9feeed710609749c3cc30ae26b1fbed0d6c
gowrimurugan <107912689+gowrimurugan@users.noreply.github.com>
2022-09-11 13:12:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+// let a = 20;
+// let b = 5;
+
+// let add = a + b;
+// let sub = a - b;
+// let multiple = a * b;
+// let div = a / b;
+
+// console.log("addition is "+ add);
+// console.log("subraction is "+ sub);
+// console.log("mulitplication is "+ multiple);
+// console.log("division is "+ div);
+
+let person = {
+    name : "priya",
+    age : 22,
+    gender : "female"
+}
+console.log(person.age);
